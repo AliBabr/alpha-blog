f0318c08d036952075c4992e385c843951b135c5
Babar Ali <babar2166@gmail.com>
2018-05-22 18:10:12 -0400

    modify readme for repo

--- rails_projects/test_app/app/controllers/articles_controller.rb
+++ rails_projects/test_app/app/controllers/articles_controller.rb
@@ -1,21 +1,54 @@
 class ArticlesController < ApplicationController
 
-  def new 
-    @article =Article.new
-    
-    
+  before_action :set_article,only: [:edit, :update, :show, :destroy]
+  def new
+    @article=Article.new
   end
-    
+  
+  def edit
+  end
+  
+  def index
+    @articles=Article.all    
+  end
+  
   def create
-    
     @article=Article.new(article_params)
-    @article.save
-    redirect_to_articles_show(@article)
+    if @article.save
+      flash[:success]="Article successfully created "
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
   
-  private
-    def article_params
-      params.require(:article).permit(:title, :description)
+  def update
+      if @article.update(article_params)
+      flash[:success]="Article was successfully updated"
+      redirect_to article_path(@article)
+      else
+      render 'edit'
     end
+   
+  end
   
+  def show 
+  end
+  
+  def destroy
+    @article.destroy
+    flash[:success]="Article was succesfuly deleted"
+    redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article=Article.find(params[:id])
+  end
+    
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
